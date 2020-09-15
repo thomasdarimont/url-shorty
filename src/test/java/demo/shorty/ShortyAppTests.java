8293208800c49704f3735e2721201d7c7f114fc5
Thomas Darimont <thomas.darimont@googlemail.com>
2020-09-16 00:50:39 +0200

    Add some unit tests

--- src/test/java/demo/shorty/ShortyAppTests.java
+++ src/test/java/demo/shorty/ShortyAppTests.java
@@ -1,13 +1,54 @@
 package demo.shorty;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class ShortyAppTests {
 
+    @Autowired
+    ShortyService shortyService;
+
     @Test
-    void contextLoads() {
+    void thereShouldBeNoUrlsOnStart() {
+        assertThat(shortyService.findAll()).isEmpty();
     }
 
+    @Test
+    void createAndFindAll() {
+
+        ShortyUrl shortUrl = shortyService.shorten("https://google.de");
+
+        List<ShortyUrl> urls = shortyService.findAll();
+        assertThat(urls).isNotEmpty();
+        assertThat(urls).hasSize(1);
+    }
+
+    @Test
+    void createAndFindOne() {
+
+        ShortyUrl shortUrl = shortyService.shorten("https://google.de");
+
+        Optional<ShortyUrl> found = shortyService.findById(shortUrl.getShortId());
+
+        assertThat(found).isNotEmpty();
+        assertThat(found).contains(shortUrl);
+    }
+
+    @Test
+    void createAndDeleteOne() {
+
+        ShortyUrl shortUrl = shortyService.shorten("https://google.de");
+
+        shortyService.deleteLink(shortUrl.getShortId());
+
+        Optional<ShortyUrl> found = shortyService.findById(shortUrl.getShortId());
+        assertThat(found).isEmpty();
+    }
 }
